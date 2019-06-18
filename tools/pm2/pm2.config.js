720d29e5a39ea191cf12fe68b0162a0559a96b3a
Kos <koshuang@gmail.com>
2019-06-18 16:03:35 +0800

    chore(config): [pm2] added

---
+++ tools/pm2/pm2.config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+module.exports = {
+  apps: [{
+    name: process.env.APPLICATION,
+    script: './app/server.js',
+    cwd: process.env.DEPLOY_TO + '/current',
+    error_file: './logs/pm2.app.log',
+    out_file: './logs/pm2.app.log',
+    exec_mode: 'cluster',
+    instances: '1',
+    wait_ready: true,
+    listen_timeout: 30000,
+    env: {
+      NODE_ENV: process.env.NODE_ENV,
+    },
+  }],
+};
