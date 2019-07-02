b2c7b4f29fd87737f57d5b967bd34cf14d657b10
l443018 <l443018@gmail.com>
2019-07-02 16:23:55 +0800

    feat(component): [*] change js component to tsx and remove AppProvider

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -2,4 +2,3 @@ export { Heading } from '@Components/Heading';
 export { default as Page } from '@Components/Page';
 export { default as Signin } from '@Components/Signin';
 export { default as Signup } from '@Components/Signup';
-export { default as AppProvider } from '@Components/shared/AppProvider';
