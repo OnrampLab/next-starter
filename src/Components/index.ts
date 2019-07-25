1997b08ec4cc08dcf9161d968a260f412d5464e7
Kos <koshuang@gmail.com>
2019-07-25 15:11:05 +0800

    feat(component): [Dashboard] added to show API result

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -2,3 +2,4 @@ export * from '@Components/Heading';
 export * from '@Components/Page';
 export * from '@Components/Signin';
 export * from '@Components/Signup';
+export * from '@Components/Dashboard';
