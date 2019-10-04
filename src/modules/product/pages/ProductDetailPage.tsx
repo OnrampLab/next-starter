3beba8f2957c1b61bb1347bd13e982ee19d69003
Kos <koshuang@gmail.com>
2019-10-04 16:44:40 +0800

    feat(product/detail): add spin effect

--- src/modules/product/pages/ProductDetailPage.tsx
+++ src/modules/product/pages/ProductDetailPage.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DailyPlanetPage } from '@onr/demo';
+import { Spin } from 'antd';
 
 export const ProductDetailPage: React.FC = () => {
+  const [done, setDone] = useState(false);
+
+  setTimeout(() => {
+    console.log('Done');
+    setDone(true);
+  }, 5000);
+
   return (
     <>
       <h1>Detail Works!</h1>
-      <DailyPlanetPage />
+
+      <Spin tip="Loading" spinning={!done}>
+        <DailyPlanetPage />
+      </Spin>
     </>
   );
 };
