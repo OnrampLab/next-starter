36667f33aabce1fa4d5ebd45b4deb5fb77e2ef04
Kos <koshuang@gmail.com>
2020-03-13 17:57:49 +0800

    refactor(tools/config): [pm2] format code

--- tools/pm2/pm2.config.js
+++ tools/pm2/pm2.config.js
@@ -1,18 +1,20 @@
 require('dotenv').config();
 
 module.exports = {
-  apps: [{
-    name: process.env.APPLICATION,
-    script: './src/server/index.js',
-    cwd: process.env.DEPLOY_TO + '/current',
-    error_file: './logs/pm2.app.log',
-    out_file: './logs/pm2.app.log',
-    exec_mode: 'cluster',
-    instances: '1',
-    wait_ready: true,
-    listen_timeout: 30000,
-    env: {
-      NODE_ENV: process.env.NODE_ENV,
+  apps: [
+    {
+      name: process.env.APPLICATION,
+      script: './src/server/index.js',
+      cwd: `${process.env.DEPLOY_TO}/current`,
+      error_file: './logs/pm2.app.log',
+      out_file: './logs/pm2.app.log',
+      exec_mode: 'cluster',
+      instances: '1',
+      wait_ready: true,
+      listen_timeout: 30000,
+      env: {
+        NODE_ENV: process.env.NODE_ENV,
+      },
     },
-  }],
+  ],
 };
