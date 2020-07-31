c7cdde1a86a36fe0be41668b3571a6b94de4147b
Paul Lee <e0991100238@gmail.com>
2020-07-31 18:23:58 +0800

    fix(Account): update to v4 type

--- src/modules/account/components/AccountForm/AccountForm.tsx
+++ src/modules/account/components/AccountForm/AccountForm.tsx
@@ -1,83 +1,92 @@
 import React from 'react';
 import { Switch, Form, Input, Button } from 'antd';
-import { FormComponentProps } from 'antd/lib/form';
+import { FormProps } from 'antd/lib/form';
 import { IAccount } from '@onr/account';
 
-interface IAccountFormProps extends FormComponentProps {
+interface IAccountFormProps extends FormProps {
   currentAccount: IAccount;
   handleSubmit: (account: IAccount) => void;
 }
 
-export const Component: React.FC<IAccountFormProps> = ({
+export const AccountForm: React.FC<IAccountFormProps> = ({
   form,
   currentAccount,
   handleSubmit,
 }: IAccountFormProps) => {
-  const { getFieldDecorator } = form;
   const formItemLayout = {
     labelCol: { span: 6 },
     wrapperCol: { span: 18 },
   };
-  const onSubmit = (e: MouseEvent) => {
-    e.preventDefault();
+  const onSubmit = (values: { account: IAccount }) => {
+    handleSubmit(values.account);
+    console.log('Received values of form: ', values);
 
-    form.validateFields(async (err, values) => {
-      if (err) {
-        return;
-      }
-      console.log('Received values of form: ', values);
-
-      handleSubmit(values.account);
-      form.resetFields();
-    });
-  };
-
-  const onEmailValidationChange = (checked: bool): void => {
-    currentAccount.email_validation_enabled = checked;
+    form?.resetFields();
   };
 
   return (
     <>
-      <Form {...formItemLayout} onSubmit={onSubmit}>
-        <Form.Item label="Name" hasFeedback>
-          {getFieldDecorator('account.name', {
+      <Form
+        {...formItemLayout}
+        onFinish={v => onSubmit(v as { account: IAccount })}
+        initialValues={{
+          account: {
+            ...currentAccount,
+            redirect_domain: currentAccount.redirect_domain || 'https://g17.net',
+          },
+        }}
+      >
+        <Form.Item
+          label="Name"
+          hasFeedback
+          name={['account', 'name']}
+          {...{
             rules: [{ required: true, message: 'Please input a name!' }],
-            initialValue: currentAccount.name,
-          })(<Input style={{ width: 400 }} />)}
+          }}
+        >
+          <Input style={{ width: 400 }} />
         </Form.Item>
 
-        <Form.Item label="Redirect Domain" hasFeedback>
-          {getFieldDecorator('account.redirect_domain', {
+        <Form.Item
+          label="Redirect Domain"
+          hasFeedback
+          name={['account', 'redirect_domain']}
+          {...{
             rules: [{ required: true, message: 'Please input a domain!' }],
-            initialValue: currentAccount.redirect_domain || 'https://g17.net',
-          })(<Input style={{ width: 400 }} />)}
+          }}
+        >
+          <Input style={{ width: 400 }} />
         </Form.Item>
 
-        <Form.Item label="Email Validation">
-          {getFieldDecorator('account.email_validation_enabled', {
-            initialValue: currentAccount.email_validation_enabled,
-          })(<Switch checked={currentAccount.email_validation_enabled} onChange={onEmailValidationChange} />)}
+        <Form.Item label="Email Validation" name={['account', 'email_validation_enabled']}>
+          <Switch />
         </Form.Item>
 
-        <Form.Item label="Logo" hasFeedback>
-          {getFieldDecorator('account.logo', {
-            rules: [{ message: 'Please input a logo!' }],
-            initialValue: currentAccount.logo,
-          })(<Input style={{ width: 400 }} />)}
+        <Form.Item
+          label="Logo"
+          hasFeedback
+          name={['account', 'logo']}
+          rules={[{ message: 'Please input a logo!' }]}
+        >
+          <Input style={{ width: 400 }} />
         </Form.Item>
 
-        <Form.Item label="Signature" hasFeedback>
-          {getFieldDecorator('account.signature', {
-            rules: [{ message: 'Please input a signature!' }],
-            initialValue: currentAccount.signature,
-          })(<Input style={{ width: 400 }} />)}
+        <Form.Item
+          label="Signature"
+          hasFeedback
+          name={['account', 'signature']}
+          rules={[{ message: 'Please input a signature!' }]}
+        >
+          <Input style={{ width: 400 }} />
         </Form.Item>
 
-        <Form.Item label="Address" hasFeedback>
-          {getFieldDecorator('account.address', {
-            rules: [{ message: 'Please input a address!' }],
-            initialValue: currentAccount.address,
-          })(<Input style={{ width: 400 }} />)}
+        <Form.Item
+          label="Address"
+          hasFeedback
+          name={['account', 'address']}
+          rules={[{ message: 'Please input a address!' }]}
+        >
+          <Input style={{ width: 400 }} />
         </Form.Item>
 
         <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
@@ -89,7 +98,3 @@ export const Component: React.FC<IAccountFormProps> = ({
     </>
   );
 };
-
-export const AccountForm = Form.create<IAccountFormProps>({
-  name: 'account_form',
-})(Component);
