bdbaa7e5d4557859834f074dd8582f8fa6c59036
Paul Lee <e0991100238@gmail.com>
2020-08-03 15:26:44 +0800

    fix(AccountList): upgrade to antd v4, usePubSub

--- src/modules/account/components/AccountList.tsx
+++ src/modules/account/components/AccountList.tsx
@@ -2,22 +2,42 @@ import React, { useState, useContext } from 'react';
 import { Switch, Table, Card, Modal, Button, message, Popconfirm } from 'antd';
 import { ColumnProps } from 'antd/lib/table';
 import { IAccount, CreateAccountForm, UpdateAccountForm, AccountService } from '@onr/account';
-import { DefaultPubSubContext } from '@onr/core';
+import { usePubSub, DefaultPubSubContext } from '@onr/core';
 
 interface IAccountListProps {
   accounts: IAccount[];
 }
 
-class AccountTable extends Table<IAccount> {}
-
 export const AccountList: React.FC<IAccountListProps> = ({ accounts }: IAccountListProps) => {
   const [currentAccount, setCurrentAccount] = useState<IAccount>(accounts[0]);
   const [createAccountModalVisible, setCreateAccountModalVisible] = useState(false);
   const [updateAccountModalVisible, setUpdateAccountModalVisible] = useState(false);
-  const { publish } = useContext(DefaultPubSubContext);
+  const { publish } = usePubSub(DefaultPubSubContext);
 
   const logoStyle = {
     maxHeight: '60px',
+  };
+
+  const openCreateDialog = () => {
+    setCreateAccountModalVisible(true);
+  };
+
+  const openEditDialog = (account: IAccount) => {
+    setCurrentAccount(account);
+    setUpdateAccountModalVisible(true);
+  };
+
+  const deleteAccount = async (account: IAccount) => {
+    try {
+      if (!account.id) {
+        throw new Error('No account id');
+      }
+      await AccountService.deleteAccount({ accountId: account.id });
+      message.success(`Account ${account.name} deleted`);
+      publish('account.updated');
+    } catch (e) {
+      message.error(`Failed to delete account${e.message && `: ${e.message}`}`);
+    }
   };
 
   const columns: ColumnProps<IAccount>[] = [
@@ -41,7 +61,8 @@ export const AccountList: React.FC<IAccountListProps> = ({ accounts }: IAccountL
             <a onClick={() => openEditDialog(account)}>Edit</a>
             <Popconfirm
               title="Confirm delete account"
-              onConfirm={() => deleteAccount(account)}>
+              onConfirm={async () => deleteAccount(account)}
+            >
               <a>Delete</a>
             </Popconfirm>
           </span>
@@ -49,28 +70,6 @@ export const AccountList: React.FC<IAccountListProps> = ({ accounts }: IAccountL
       },
     },
   ];
-
-  const openCreateDialog = () => {
-    setCreateAccountModalVisible(true);
-  };
-
-  const openEditDialog = (account: IAccount) => {
-    setCurrentAccount(account);
-    setUpdateAccountModalVisible(true);
-  };
-
-  const deleteAccount = async (account: IAccount) => {
-    try {
-      if (!account.id) {
-        throw new Error('No account id');
-      }
-      await AccountService.deleteAccount({ accountId: account.id });
-      message.success(`Account ${account.name} deleted`);
-      publish('account.updated');
-    } catch (e) {
-      message.error(`Failed to delete account${e.message && `: ${e.message}`}`);
-    }
-  }
 
   return (
     <>
@@ -82,7 +81,7 @@ export const AccountList: React.FC<IAccountListProps> = ({ accounts }: IAccountL
           </Button>
         }
       >
-        <AccountTable rowKey="id" columns={columns} dataSource={accounts} pagination={false} />
+        <Table<IAccount> rowKey="id" columns={columns} dataSource={accounts} pagination={false} />
       </Card>
 
       {createAccountModalVisible && (
