888d311d4ffd8648cd614401a604d643a9884ab4
Paul Lee <e0991100238@gmail.com>
2020-08-03 15:25:28 +0800

    fix(core):[usePubSub] fix type incorrect, enhanced hinting

--- src/modules/core/hooks/usePubSub.ts
+++ src/modules/core/hooks/usePubSub.ts
@@ -1,13 +1,43 @@
-import { createContext, useReducer, useEffect, useRef } from 'react';
+import { createContext, useReducer, useEffect, useRef, Context } from 'react';
+
+type TChannelName = string | number;
+
+interface InitialState {
+  channels: Record<TChannelName, Function[]>;
+}
+
+type TActions =
+  | {
+      type: 'Subscribe' | 'Unsubscribe';
+      channel: TChannelName;
+      handler: Function;
+    }
+  | {
+      type: 'Publish';
+      message?: Record<string, any>;
+      channel: TChannelName;
+    };
+
+type TPublishFunction = <T>(
+  channel: TChannelName,
+  message?: T extends undefined ? Record<string, any> : T,
+) => void | Function;
+
+type TSubscribingFunction = <T>(
+  channel: TChannelName,
+  handler: T extends Function ? T : Function,
+) => void | Function;
 
 //Provide an app wide pub sub context
-export const DefaultPubSubContext = createContext();
-export const usePubSub = context => {
-  let PubSubContext = DefaultPubSubContext;
+export const DefaultPubSubContext = createContext(null);
+
+export const usePubSub = <T>(context: T extends Context<{}> ? T : Context<T>) => {
+  let PubSubContext: unknown;
 
   if (context !== undefined) PubSubContext = context;
-  const [subscriptionManager, dispatch] = useReducer(
-    (state, action) => {
+  else PubSubContext = DefaultPubSubContext;
+  const [_, dispatch] = useReducer(
+    (state: InitialState, action: TActions) => {
       const current = { ...state };
       switch (action.type) {
         case 'Subscribe':
@@ -48,16 +78,18 @@ export const usePubSub = context => {
     ref.current = { dispatch };
   }, [dispatch]);
 
-  const subscribe = (channel, handler) => {
+  const unsubscribe: TSubscribingFunction = (channel, handler) => {
+    if (!ref.current) return () => {};
+    ref.current.dispatch({ type: 'Unsubscribe', channel, handler });
+  };
+
+  const subscribe: TSubscribingFunction = (channel, handler) => {
     if (!ref.current) return () => {};
     ref.current.dispatch({ type: 'Subscribe', channel, handler });
     return () => unsubscribe(channel, handler);
   };
-  let unsubscribe = (channel, handler) => {
-    if (!ref.current) return () => {};
-    ref.current.dispatch({ type: 'Unsubscribe', channel, handler });
-  };
-  const publish = (channel, message) => {
+
+  const publish: TPublishFunction = (channel, message) => {
     if (!ref.current) return () => {};
     ref.current.dispatch({ type: 'Publish', channel, message });
   };
