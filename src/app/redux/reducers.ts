cfb79bcf3f1a36e696225a5814279871de939d6d
l443018 <l443018@gmail.com>
2020-08-05 15:09:46 +0800

    feat(app/redux): add account and auth reducers

--- src/app/redux/reducers.ts
+++ src/app/redux/reducers.ts
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 
 import { reducers as wrapperReducers } from '@onr/core';
+import { reducers as accountReducers } from '@onr/account';
+import { reducers as authReducers } from '@onr/auth';
 
 export default combineReducers({
   ...wrapperReducers,
+  ...accountReducers,
+  ...authReducers,
 });
