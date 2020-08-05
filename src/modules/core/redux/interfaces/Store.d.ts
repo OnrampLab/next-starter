4ac6154fa953b00969f10d9a9fc943839eb5220e
l443018 <l443018@gmail.com>
2020-08-05 15:30:36 +0800

    feat(core/redux): add auth store to IStore

--- src/modules/core/redux/interfaces/Store.d.ts
+++ src/modules/core/redux/interfaces/Store.d.ts
@@ -1,7 +1,11 @@
 import { IAccount } from '@onr/account';
+import { IUser } from '@onr/user';
 
 export interface IStore {
   accountStore: {
     accounts: IAccount[];
+  };
+  authStore: {
+    currentUser: IUser;
   };
 }
